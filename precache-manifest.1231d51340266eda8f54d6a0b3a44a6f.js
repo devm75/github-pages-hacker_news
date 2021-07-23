5c2a95c350241ba2b7a8c708e4b89767c3a2a0a2
Mohan Dev <bhardwaj1206409@gmail.com>
2021-07-23 17:00:44 +0530

    Updates

---
+++ precache-manifest.1231d51340266eda8f54d6a0b3a44a6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a0d0096bde074efb73470937ee5019",
+    "url": "/github-pages-hacker_news/index.html"
+  },
+  {
+    "revision": "242df1677419e66879da",
+    "url": "/github-pages-hacker_news/static/css/main.9c59207b.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/github-pages-hacker_news/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-hacker_news/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242df1677419e66879da",
+    "url": "/github-pages-hacker_news/static/js/main.2dba2845.chunk.js"
+  },
+  {
+    "revision": "af26b77d61003c8b5c09",
+    "url": "/github-pages-hacker_news/static/js/runtime-main.90fac139.js"
+  }
+]);
